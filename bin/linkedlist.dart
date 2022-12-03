8db095b0b19318a3a3ca64e105b44f8c605cefcb
TAYAB009 <tayabfarooq009@gmail.com>
2022-12-03 15:47:50 +0500

    linked-List

---
+++ bin/linkedlist.dart
@@ -0,0 +1,5 @@
+class Node<T> {
+  T value;
+  Node<T>? next;
+  Node({required this.value, this.next});
+}
---
+++ test/linkedlist_test.dart
@@ -0,0 +1,8 @@
+// import 'package:linkedlist/linkedlist.dart';
+// import 'package:test/test.dart';
+
+// void main() {
+//   test('calculate', () {
+//     expect(calculate(), 42);
+//   });
+// }
